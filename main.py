64841c02093210792169b83de81f3cb900100fd9
capt-marwil <mrgwillemsen@gmail.com>
2016-02-04 17:31:04 +0100

    ADD: Facebook Login!!!

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def gconnect():
     if getUserID(login_session['email']):
         getUserInfo(getUserID(login_session['email']))
     else:
-        CreateUser(login_session)
+        createUser(login_session)
 
 
     output = ''
@@ -209,9 +209,95 @@ def gdisconnect():
         return response
 
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+
+
+    app_id = json.loads(open('fb_client_secret.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secret.json', 'r').read())['web']['app_secret']
+
+    url = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s" % (app_id, app_secret, access_token)
+
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    """ Use token to get user info from API """
+    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    """ strip expire tag from access token """
+    token = result.split("&")[0]
+
+
+    url = 'https://graph.facebook.com/v2.5/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # DEBUG output
+    print "url sent for API access: %s" % url
+    print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # get user picture
+
+    url = "https://graph.facebook.com/v2.5/me/picture?%s&redirect=0&height=200&width=200" % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists in local database
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+
+@app.route('/fbdisconnet')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    del login_session['username']
+    del login_session['email']
+    del login_session['picture']
+    del login_session['facebook_id']
+    return "You have been logged out"
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/disconnet')
+def disconnet():
+    pass
 
 """ routes to add, edit and delete database entities require a valid login """
 
@@ -327,7 +413,7 @@ def getUserInfo(user_id):
     return user
 
 
-def CreateUser(login_session):
+def createUser(login_session):
     NewUser = User(name=login_session['username'],
                    email=login_session['email'],
                    picture=login_session['picture'])
